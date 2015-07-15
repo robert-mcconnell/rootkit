76e65e4450dbcc787d64e2b5ad99f14580554484
Albert Faust <robert.mcconnell3@mail.dcu.ie>
2015-07-15 14:00:11 +0100

    Update brute.sh
    Works for 1 digit ascii password.

--- brute.sh
+++ brute.sh
@@ -1,7 +1,8 @@
-echo "Enter 4 digit Password:"                                                                                                                              
+echo "Enter 1 digit Password:"
 read passwd
-for i in `seq 1 9999`i; do
-  if [ $passwd == $i ]; then
-    echo Password is: $i
-  fi
+for i in `seq 32 127`; do
+  oct=$(printf \\$(printf "%o" $i))
+  if [ $passwd == $oct ]; then                                                                                                                            
+    echo Password is: $oct
+  fi  
 done
