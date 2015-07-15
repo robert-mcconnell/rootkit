251e1cb4b8c1b9298e27eccefb07b9124ccd90ff
Albert Faust <robert.mcconnell3@mail.dcu.ie>
2015-07-15 17:11:15 +0100

    Update brute.sh
    Works for ascii password of any lenght.

--- brute.sh
+++ brute.sh
@@ -1,8 +1,12 @@
-echo "Enter 1 digit Password:"
+echo "Enter Password:"
 read passwd
-for i in `seq 32 127`; do
-  oct=$(printf \\$(printf "%o" $i))
-  if [ $passwd == $oct ]; then                                                                                                                            
-    echo Password is: $oct
-  fi  
+for ((j=0;j<${#passwd};j++)); do
+  for i in `seq 32 127`; do
+    oct=$(printf \\$(printf "%o" $i))
+    pass[$j]="${passwd:j:1}"
+    if [ ${pass[$j]} == "$oct" ]; then                                                                                                                  
+      cracked+=$oct
+    fi
+  done
 done
+echo "Password is: $cracked"
