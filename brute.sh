3a35ec56a142e44fed045955f02896535b7602c2
Albert Faust <robert.mcconnell3@mail.dcu.ie>
2015-07-16 23:20:33 +0100

    Update brute.sh

--- brute.sh
+++ brute.sh
@@ -20,9 +20,26 @@ for ((j=0;j<$passlen;j++)); do
         echo $oct
         if [ ${passwd} == "$oct" ]; then
           cracked+=$oct
-          echo "Password is: $cracked"
-          exit
         fi
+      done
+    done
+  fi
+  if [ -n "$cracked" ]; then
+    echo "Password is: $cracked"
+    exit
+  else
+    for i in `seq 32 127`; do
+      for j in `seq 32 127`; do
+        for k in `seq 32 127`; do
+          oct=$(printf \\$(printf "%o" $i))$(printf \\$(printf "%o" $j))$(printf \\$(printf "%o" $k))
+          echo $oct
+          if [ ${passwd} == "$oct" ]; then
+            echo $oct
+            cracked+=$oct
+            echo "Password is: $cracked"
+            exit
+          fi
+        done
       done
     done
   fi
